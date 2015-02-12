7cafc951004e323749e7692b9637a44e09c5ee97
tcwijekoon <tcwijekoon@gmail.com>
2015-02-12 23:31:58 +0530

    facade

---
+++ src/FacadePattern/Rectangle.java
@@ -0,0 +1,11 @@
+package FacadePattern;
+
+/**
+ * Created by thushara.wijekoon on 2/11/2015.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
